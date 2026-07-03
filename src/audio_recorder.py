ac7893686012387a108789a089def6835432dc0d
Miquel E.M <estmiqueltbo@gmail.com>
2026-07-03 23:12:44 +0200

    feat: Fourier Transform Created

--- src/audio_recorder.py
+++ src/audio_recorder.py
@@ -1,1 +1,4 @@
-
+import queue
+import sys
+import numpy as np
+import sounddevice as sd
--- src/fourier_transform.py
+++ src/fourier_transform.py
@@ -1,1 +1,13 @@
+import numpy as np
+from scipy.fft import rfft, fftfreq
 
+def process_audio(audio, sample_rate):
+    if len(audio.Shape):
+        np.mean(audio, axis=1)
+
+    audio_transform = rfft(audio)
+
+    magnitude = np.abs(audio_transform) # type: ignore
+    frequencies = fftfreq(audio_transform, d = 1.0/sample_rate)
+
+    return magnitude, frequencies
